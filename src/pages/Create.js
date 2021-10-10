c79048afc3b159d83dc22510c903e29740cc5fb3
Rohan Shivesh <rvss28@gmail.com>
2021-10-10 16:26:27 +0530

    add icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,7 +3,9 @@ import { Typography } from '@mui/material'
 import { Button } from '@mui/material'
 import { ButtonGroup } from '@mui/material'
 import { Container } from '@mui/material'
-
+import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
 export default function Create() {
   return (
     <Container>
@@ -21,25 +23,23 @@ export default function Create() {
         color="secondary"
         variant="contained"
         onClick={() => console.log("youi clicked me")}
+
+        // startIcon={<SendOutlinedIcon />}
+        endIcon={<KeyboardArrowRightOutlinedIcon />}
+
       >Submit</Button>
+      <br />
 
-      {/* <Button type="submit" color="primary">Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
 
-      <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-        <Button>Four</Button>
-      </ButtonGroup> */}
+
     </Container>
   )
 }
 
-//       Create page
-{/* <Typography variant="h1" color="secondary" align="center" > */ }
-// Create a new note
-// </Typography>
-//<Typography color="error" align="center" noWrap>{/*noWrap makes sure that the text does not go onto the next line and stays on the same line*/}
-//Create a new note  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, quas aut. Id quibusdam, minus nulla, sint maxime amet, saepe laudantium repudiandae explicabo error labore molestiae neque nemo ratione eum ipsum mollitia. Iusto eum animi saepe iure quo, error a cumque molestiae quas doloribus aperiam illum expedita iste, dolorem tempore assumenda?
-// </Typography>
+
+{/* <AcUnitOutlinedIcon />
+<AcUnitOutlinedIcon color="primary" fontSize="large" />
+<AcUnitOutlinedIcon color="secondary" fontSize="small" />
+<AcUnitOutlinedIcon color="action" fontSize="small" />
+<AcUnitOutlinedIcon color="error" fontSize="small" />
+<AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
