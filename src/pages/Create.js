99a3bd5e95bd423439d83c84f3d04ef2312e85f9
Rohan Shivesh <rvss28@gmail.com>
2021-10-10 19:12:04 +0530

    add makeStyles

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,7 +6,38 @@ import { Container } from '@mui/material'
 import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
+import { makeStyles } from '@mui/styles';
+// import { makeStyles } from '@material-ui/core/styles'
+
+// import { makeStyles } from '@mui/';
+
+
+
+const useStyles = makeStyles({
+  btn1: {
+
+    height: 60,
+    '&:hover': {
+      height: 100,
+    }
+
+  },
+
+  typo: {
+    color: 'orange',
+  },
+
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+
+});
+
 export default function Create() {
+
+  const classes = useStyles();
+
   return (
     <Container>
 
@@ -15,19 +46,21 @@ export default function Create() {
         variant="h6"
         component="h2"
         gutterBottom
-        color="textSecondary"
+        // color="textSecondary"
+        className={classes.typo}
       >
         Create a New Note
       </Typography>
-      <Button type="submit"
-        color="secondary"
-        variant="contained"
+      <Button
+        type="submit"
+
+        className={classes.btn1}
+        variant="outlined"
         onClick={() => console.log("youi clicked me")}
 
-        // startIcon={<SendOutlinedIcon />}
         endIcon={<KeyboardArrowRightOutlinedIcon />}
 
-      >Submit</Button>
+      >Submit {console.log(classes)}</Button>
       <br />
 
 
